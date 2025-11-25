9bf6b233f5f2287a2fcf363adf247c298dd6cd72
adarsh <adarshshanu3@gmail.com>
2025-11-25 08:05:44 +0530

    some animation

---
+++ react-features/src/components/Card.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+interface ItemInterface {
+  image: string;
+  title: string;
+  price: number;
+}
+interface CardInterface {
+  item: ItemInterface;
+}
+const Card: React.FC<CardInterface> = ({ item }) => {
+  return (
+    <div
+      className={`card border w-[20rem] p-4 bg-[#ffffff] text-[#6e6b6b] rounded-xl transition-all duration-300 hover:scale-105 hover:shadow-[0px_0px_50px_9px_rgba(0,0,0,0.3)] shadow-[#39fc12]`}
+      // className="card border w-[20rem] p-4 bg-[#ffffff] text-[#6e6b6b] rounded-xl transition-transform duration-800 hover:-translate-y-3 hover:shadow-[0px_0px_50px_9px_rgba(0,0,0,0.3)] shadow-[#fa2323]"
+    >
+      <img className="h-80" src={item?.image} alt="product image" />
+      <h2>{item?.title}</h2>
+      <p className="font-semibold text-black">{item?.price}</p>
+      <div className="w-full flex justify-around items-center">
+        <div className="h-12 w-12 bg-yellow-300 animate-spin"></div>
+        <div className="h-12 w-12 bg-blue-300 animate-bounce"></div>
+        <div className="h-12 w-12 bg-red-300 animate-ping"></div>
+        <div className="h-12 w-12 bg-green-300 animate-pulse"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- react-features/src/pages/ItemPage.tsx
+++ react-features/src/pages/ItemPage.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useTransition } from "react";
+import Card from "../components/Card";
 
 const ItemPage = () => {
   const [isPending, startTransition] = useTransition();
@@ -27,24 +28,10 @@ const ItemPage = () => {
         <div>Loadingl...</div>
       ) : (
         <div className="flex justify-center">
-          {data && data.length > 0 ? (
+          {data && data?.length > 0 ? (
             <div className="flex flex-wrap justify-around py-5 gap-4 w-[80%]">
               {data?.map((item: any, index: number) => (
-                <div
-                  className={`card border w-[20rem] p-4 bg-[#ffffff] text-[#6e6b6b] rounded-xl transition-all duration-300 hover:scale-105 hover:shadow-[0px_0px_50px_9px_rgba(0,0,0,0.3)] shadow-[#39fc12]`}
-                  // className="card border w-[20rem] p-4 bg-[#ffffff] text-[#6e6b6b] rounded-xl transition-transform duration-800 hover:-translate-y-3 hover:shadow-[0px_0px_50px_9px_rgba(0,0,0,0.3)] shadow-[#fa2323]"
-                  key={`product-${index}`}
-                >
-                  <img className="h-80" src={item?.image} alt="product image" />
-                  <h2>{item?.title}</h2>
-                  <p className="font-semibold text-black">{item?.price}</p>
-                  <div className="w-full flex justify-around items-center">
-                    <div className="h-12 w-12 bg-yellow-300 animate-spin"></div>
-                    <div className="h-12 w-12 bg-blue-300 animate-bounce"></div>
-                    <div className="h-12 w-12 bg-red-300 animate-ping"></div>
-                    <div className="h-12 w-12 bg-green-300 animate-pulse"></div>
-                  </div>
-                </div>
+                <Card key={`card-${index}`} item={item} />
               ))}
             </div>
           ) : (
